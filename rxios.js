b2c5e39334c08be9ff12af50df338856ee117dbe
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-09-14 19:00:42 +0900

    feat: RXIOS - 라이브러리 & API 관심사 분리

--- rxios.js
+++ rxios.js
@@ -1,22 +1,38 @@
 class Rxios {
     constructor() {
-        this._url = "https://isa-myblog.herokuapp.com/post/";
+
     }
-    async _rxios(api) {
+    async _rxios(api, method, data) {
         try {
-            const response = await fetch(api);
-            const data = await response.json();
-            return data;
+            const request = await fetch(api, {
+                method: method,
+                body: data
+            });
+            const response = await request.json();
+            return response;
         } catch (e) {
             console.error(e);
         }
 
     }
+    async get(url) {
+        return await this._rxios(url, "GET");
+    }
+    async post(url, data) {
+        return await this._rxios(url, "POST", data);
+    }
+}
+
+class Api {
+    constructor() {
+        this.rxios = new Rxios();
+        this._url = "https://isa-myblog.herokuapp.com/post/";
+    }
     async getTags() {
-        const result = await this._rxios(`${this._url}tags`);
+        const result = await this.rxios.get(`${this._url}tags`);
         console.log(result);
     }
 }
 
-const api = new Rxios();
+const api = new Api();
 api.getTags();
